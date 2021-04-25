f307167f5f6563a2aa57b45b69a3858d97c4949e
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2021-04-25 13:43:14 +0200

    feat: remote compilation

---
+++ compile.js
@@ -0,0 +1,41 @@
+const axios = require('axios').default;
+const fs = require('fs');
+const rimraf = require('rimraf');
+
+
+
+const ip = "http://localhost:3666/api";
+
+async function main() {
+    try {
+        const filename = fs.readdirSync("./workspace").find(v => v.endsWith("_module.cpp"));
+        const projectName = filename.replace("_module.cpp", "");
+        const f = axios.post(ip + "/compiled.so", {
+            python: fs.readFileSync("./workspace/setup.py").toString(),
+            cpp: fs.readFileSync("./workspace/" + filename).toString(),
+            filename
+        });
+        if (fs.existsSync("./build/")) await rm("./build/");
+
+        fs.mkdirSync("./build/");
+
+        fs.copyFileSync("./workspace/" + projectName + "/__init__.py", "./build/" + projectName + ".py");
+        fs.writeFileSync("./build/" + projectName + ".py", fs.readFileSync("./build/" + projectName + ".py").toString().replace(".dll", ".py"));
+        
+
+        fs.copyFileSync("./workspace/main.py", "./build/main.py");
+
+        fs.writeFileSync("./build/lib.py", (await f).data);
+
+    } catch (e) {
+        // console.log(e);
+    }
+}
+function rm(pth) {
+    return new Promise((resolve, reject) => {
+        rimraf(pth, () => {
+            resolve();
+        });
+    });
+}
+main();
